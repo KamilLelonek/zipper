41124dfaae769d75eb1dbc5f5f34eb173f77308c
Kamil Lelonek <squixy.sln@gmail.com>
2019-08-23 20:55:53 +0200

    Test download options

--- lib/zipper/domain.ex
+++ lib/zipper/domain.ex
@@ -1,2 +1,19 @@
 defmodule Zipper.Domain do
+  alias Zipper.Domain.{Processor, Filename, Agent}
+
+  def create_archive(files) do
+    with archive_name <- Filename.generate(),
+         :ok <- Agent.mark_started(archive_name),
+         :ok <- Processor.create_archive(files, archive_name) do
+      archive_name
+    end
+  end
+
+  def can_download?(archive_name) do
+    cond do
+      Agent.status(archive_name) == false -> {:error, :not_ready}
+      File.exists?(archive_name) -> {:ok, archive_name}
+      true -> {:error, :not_found}
+    end
+  end
 end
--- test/zipper/domain_test.exs
+++ test/zipper/domain_test.exs
@@ -1,3 +1,50 @@
 defmodule Zipper.DomainTest do
   use Zipper.TestCase, async: true
+
+  alias Zipper.Domain
+  alias Zipper.Domain.Agent
+
+  @archive_name "archive"
+
+  setup do
+    Agent.invalidate()
+
+    on_exit(fn -> File.rm(@archive_name) end)
+
+    :ok
+  end
+
+  describe "can_download?/1" do
+    test "should report not found" do
+      assert {:error, :not_found} = Domain.can_download?(@archive_name)
+    end
+
+    test "should report not ready" do
+      Agent.mark_started(@archive_name)
+
+      assert {:error, :not_ready} = Domain.can_download?(@archive_name)
+    end
+
+    test "should report ready" do
+      Agent.mark_finished(@archive_name)
+
+      File.touch(@archive_name)
+
+      assert {:ok, @archive_name} = Domain.can_download?(@archive_name)
+    end
+
+    test "should report found" do
+      File.touch(@archive_name)
+
+      assert {:ok, @archive_name} = Domain.can_download?(@archive_name)
+    end
+  end
+
+  # def can_download?(archive_name) do
+  #   cond do
+  #     not Agent.status(archive_name) -> {:error, :not_ready}
+  #     File.exists?(archive_name) -> {:ok, archive_name}
+  #     true -> {:error, :not_found}
+  #   end
+  # end
 end
