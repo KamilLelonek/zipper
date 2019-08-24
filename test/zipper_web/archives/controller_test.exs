cc566ac58e9316b29f705b5e2f626de7ced23459
Kamil Lelonek <squixy.sln@gmail.com>
2019-08-24 21:54:15 +0200

    Test Controller download

--- test/zipper_web/archives/controller_test.exs
+++ test/zipper_web/archives/controller_test.exs
@@ -1,5 +1,17 @@
 defmodule ZipperWeb.Archives.ControllerTest do
-  use Zipper.TestCase, async: true
+  use Zipper.TestCase, async: false
+
+  alias Zipper.Domain.Agent
+
+  @archive_name "archive"
+
+  setup do
+    Agent.invalidate()
+
+    on_exit(fn -> File.rm(@archive_name) end)
+
+    :ok
+  end
 
   describe "upload" do
     test "should upload a valid JSON", %{conn: conn} do
@@ -21,5 +33,26 @@ defmodule ZipperWeb.Archives.ControllerTest do
   end
 
   describe "download" do
+    test "should download file", %{conn: conn} do
+      File.touch(@archive_name)
+
+      assert conn
+             |> get(Routes.archives_path(conn, :download, @archive_name))
+             |> response(:ok)
+    end
+
+    test "should respond with not ready", %{conn: conn} do
+      Agent.mark_started(@archive_name)
+
+      assert conn
+             |> get(Routes.archives_path(conn, :download, @archive_name))
+             |> response(:accepted)
+    end
+
+    test "should respond with not found", %{conn: conn} do
+      assert conn
+             |> get(Routes.archives_path(conn, :download, @archive_name))
+             |> response(:not_found)
+    end
   end
 end
