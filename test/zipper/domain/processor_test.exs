40f2f709925d3e5f570c46a585376194c07cdb56
Kamil Lelonek <squixy.sln@gmail.com>
2019-08-24 18:52:27 +0200

    Extend Processor tests

--- test/zipper/domain/processor_test.exs
+++ test/zipper/domain/processor_test.exs
@@ -1,14 +1,18 @@
 defmodule Zipper.Domain.ProcessorTest do
-  use Zipper.TestCase, async: true
+  use Zipper.TestCase, async: false
 
   alias Zipper.Domain.{Processor, Agent}
 
-  @archive_name "archive"
+  @archive_name "archive.zip"
 
   setup do
+    Agent.invalidate()
+
     on_exit(fn -> File.rm(@archive_name) end)
 
-    :ok
+    bypass = Bypass.open()
+
+    {:ok, bypass: bypass}
   end
 
   describe "handle_cast/2" do
@@ -20,6 +24,27 @@ defmodule Zipper.Domain.ProcessorTest do
 
       assert Agent.status(@archive_name)
       assert :ok = File.rm(@archive_name)
+    end
+
+    test "should download file and prepare archive", %{bypass: bypass} do
+      file_name = "file.txt"
+      file_contents = "contents"
+
+      Bypass.expect_once(bypass, fn conn ->
+        Plug.Conn.resp(conn, 404, file_contents)
+      end)
+
+      Processor.handle_cast(
+        {:create_archive,
+         [%{"url" => "http://localhost:#{bypass.port}/", "filename" => file_name}],
+         @archive_name},
+        []
+      )
+
+      :zip.unzip('#{@archive_name}')
+
+      assert {:ok, ^file_contents} = File.read(file_name)
+      assert :ok = File.rm(file_name)
     end
   end
 end
